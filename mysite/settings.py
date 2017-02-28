a26bb5a2a1313d3a1a8ad6fab9c10c57bfcc0bc2
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-02-27 22:32:51 -0800

    Set up remote MySQL DB and connected vjk app to it.

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'VJK',
+        'HOST': 'vjk.c9vzvqoxblsh.us-west-2.rds.amazonaws.com',
+        'PORT':'3306',
+        'USER':'ctc_vjk',
+        'PASSWORD': 'codethechange'
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Vancouver'
 
 USE_I18N = True
 
