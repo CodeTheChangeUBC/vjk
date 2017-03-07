44f85b6bc180d84ad910f1905f3b6ac915cf0a41
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-03-06 16:12:53 -0800

    Implemented Student Model.

--- vjk/models.py
+++ vjk/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+	email = models.EmailField()
+	location = models.CharField(max_length = 100)
+	school = models.CharField(max_length = 50)
+	year_attended = models.IntegerField()
