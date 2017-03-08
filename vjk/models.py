c1d1c900516672f2c42631c0a4aeb038ceb0cb00
wchan <Because1215>
2017-03-07 19:19:07 -0800

    implemented donor model.

--- vjk/models.py
+++ vjk/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Donor(models.Model):
+	org_name = models.CharField(max_length = 50)
+	location = models.CharField(max_length = 100)
+	year_donation = models.IntegerField()
+	amount_donation = models.IntegerField()
+	contact_ID1 = models.IntegerField()
+	contact_ID2 = models.IntegerField()
