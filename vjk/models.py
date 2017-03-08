57bf059550ecf866d50a9d3cf1c16dbeff514eca
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-03-07 19:54:48 -0800

    Updated fields.

--- vjk/models.py
+++ vjk/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-	first_name = models.CharField(max_length = 30)
-	last_name = models.CharField(max_length = 30)
+	first_name = models.CharField(max_length = 75)
+	last_name = models.CharField(max_length = 75)
 	email = models.EmailField()
 	location = models.CharField(max_length = 100)
-	school = models.CharField(max_length = 50)
+	school = models.CharField(max_length = 75)
 	year_attended = models.IntegerField()
-	reference_fname = models.CharField(max_length=30,default="")
-	reference_lname = models.CharField(max_length=30,default="")
+	reference_fname = models.CharField(max_length=75,default="")
+	reference_lname = models.CharField(max_length=75,default="")
 	reference_email = models.EmailField(default="")
