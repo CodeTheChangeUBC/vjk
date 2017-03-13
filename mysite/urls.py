5d746514a5d562383f7157a60cea1904b3def95c
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-03-12 22:02:54 -0700

    Added urls for new pages.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^vjk/', include('vjk.urls')),
+	url(r'', include('vjk.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- vjk/urls.py
+++ vjk/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^home/', views.index, name='index'),
+    url(r'^search/', views.index, name='index'),
+    url(r'^email/', views.index, name='index'),
+    url(r'^edit-entries/', views.index, name='index'),
+    url(r'^add-entries/', views.index, name='index'),
+    url(r'^delete-entries/', views.index, name='index'),
 ]
