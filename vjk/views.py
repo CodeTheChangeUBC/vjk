6b6c9e1bc675e405874ec87f268d6875319b3d71
leesw98 <win981026@gmail.com>
2017-03-21 18:14:59 -0700

    First branch commit

--- vjk/views.py
+++ vjk/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, Context
+
+from .models import Sponsors
+from .models import Donor
+from .models import Student
+from .models import Volunteers
+from .models import Contacts
 
 # Create your views here.
 def search(request):
-	return HttpResponse("this page will allow you to search through tables.")
+	latest_sponsor_list = Sponsors.objects.all()
+	latest_donor_list = Donor.objects.all()
+    	template = loader.get_template("search.html")
+    	context = {
+	        'latest_sponsor_list': latest_sponsor_list,
+			'latest_donor_list': latest_donor_list
+	    }
+	return HttpResponse(template.render(context, request))
 
 def home(request):
 	return HttpResponse("you can choose what you want to do.")
@@ -15,6 +29,7 @@ def edit(request):
 	return HttpResponse("edit entries in tables.")
 
 def add(request):
+
 	return HttpResponse("add entries in tables.")
 
 def delete(request):
