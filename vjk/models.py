7ae5a53d92937184a2c01a619696e83b474c438f
leesw98 <win981026@gmail.com>
2017-03-26 23:01:26 -0700

    DB Tables except Sponsors (db error)
    TODO: Buttons, inputs, filter, more fanciness, then done

--- vjk/models.py
+++ vjk/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+	first_name = models.CharField(max_length = 75)
+	last_name = models.CharField(max_length = 75)
+	email = models.EmailField()
+	phone = models.CharField(max_length  = 20)
+	donor = models.BooleanField()
+	sponsor = models.BooleanField()
+	student = models.BooleanField()
+	volunteer = models.BooleanField()
 
-class Sponsors(models.Model):
-	name = models.CharField(max_length = 30)
-	service_provided = models.CharField(max_length = 50)
-	personal_contact = models.IntegerField()
-	sec_contact = models.IntegerField()
-
-class Donor(models.Model):
+class Donors(models.Model):
 	org_name = models.CharField(max_length = 50)
 	location = models.CharField(max_length = 100)
 	year_donation = models.IntegerField()
 	amount_donation = models.IntegerField()
-	contact_ID1 = models.IntegerField()
-	contact_ID2 = models.IntegerField()
+	primary_contact = models.ForeignKey('Contacts', on_delete=models.PROTECT, related_name="+", default=-1)
+	secondary_contact = models.ForeignKey('Contacts', on_delete=models.PROTECT, related_name="+", default=-1)
 
-class Student(models.Model):
+class Sponsors(models.Model):
+	name = models.CharField(max_length = 30)
+	service_provided = models.CharField(max_length = 50)
+	primary_contact = models.ForeignKey('Contacts', on_delete=models.PROTECT, related_name="+", default=-1)
+	secondary_contact = models.ForeignKey('Contacts', on_delete=models.PROTECT, related_name="+", default=-1)
+
+class Students(models.Model):
 	first_name = models.CharField(max_length = 75)
 	last_name = models.CharField(max_length = 75)
 	email = models.EmailField()
@@ -33,14 +42,4 @@ class Volunteers(models.Model):
 	email = models.EmailField()
 	phone = models.CharField(max_length  = 20)
 	role = models.CharField(max_length = 75)
-	years_helped = models.IntegerField()
-
-class Contacts(models.Model):
-	first_name = models.CharField(max_length = 75)
-	last_name = models.CharField(max_length = 75)
-	email = models.EmailField()
-	phone = models.CharField(max_length  = 20)
-	donor = models.BooleanField()
-	sponsor = models.BooleanField()
-	student = models.BooleanField()
-	volunteer = models.BooleanField()
+	years_helped = models.CharField(max_length=75,default="")
--- vjk/views.py
+++ vjk/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, Context
 
-from .models import Sponsors
-from .models import Donor
-from .models import Student
-from .models import Volunteers
 from .models import Contacts
+from .models import Donors
+from .models import Sponsors
+from .models import Students
+from .models import Volunteers
 
 # Create your views here.
 def search(request):
-	latest_sponsor_list = Sponsors.objects.all()
-	latest_donor_list = Donor.objects.all()
+	contacts_list = Contacts.objects.all()
+	donors_list = Donors.objects.all()
+	# sponsors_list = Sponsors.objects.all()
+	students_list = Students.objects.all()
+	volunteers_list = Volunteers.objects.all()
+
     	template = loader.get_template("search.html")
     	context = {
-	        'latest_sponsor_list': latest_sponsor_list,
-			'latest_donor_list': latest_donor_list
+	        "contacts_list": contacts_list,
+			"donors_list": donors_list,
+			# "sponsors_list": sponsors_list,
+			"students_list": students_list,
+			"volunteers_list": volunteers_list
 	    }
 	return HttpResponse(template.render(context, request))
 
