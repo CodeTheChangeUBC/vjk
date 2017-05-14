f0994b3a550a231c86107369daab2274e7a5aa21
leesw98 <win981026@gmail.com>
2017-05-14 14:53:00 +0900

    Email functionality

--- vjk/urls.py
+++ vjk/urls.py
@@ -4,17 +4,7 @@ from . import views
 
 app_name = 'vjk'
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'', views.home, name='home'),
-    url(r'^search/', views.search, name='search'),
-    url(r'^email/', views.email, name='email'),
-    url(r'^edit-entries/', views.edit, name='edit'),
-    url(r'^add-entries/', views.add, name='add'),
-    url(r'^delete-entries/', views.delete, name='delete'),
-]
-=======
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search, name='search'),
-    url(r'^email/', views.email, name='email'),
+    # url(r'^delete-entries/', views.delete, name='delete')
 ]
->>>>>>> master
--- vjk/views.py
+++ vjk/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, Context
-<<<<<<< HEAD
 from django.core import mail
-=======
 from django.views import generic
 from django.contrib.auth.decorators import login_required
->>>>>>> master
 
 from .models import Contact, Donor, Sponsor, Student, Volunteer
 
@@ -42,44 +39,26 @@ def search(request):
 			"students_list": students_list,
 			"volunteers_list": volunteers_list
 	    	}
-	return HttpResponse(template.render(context, request))
-
-
-
-def email(request):
-	connection = mail.get_connection()
-	connection.open()
-	receiver_list = request.POST["emails"]
-	sender_email = "win981026@gmail.com"
-	title = request.POST["title"]
-	body = request.POST["body"]
-	email = mail.EmailMessage(title, body, sender_email, receiver_list, connection=connection)
-	email.send()
-
-	template = loader.get_template("email.html")
-	context = {
-		"length" : receiver_list.size
-	}
-	return HttpResponse(template.render(context,request))
-
-def delete(request):
-    if request.POST:
-        table = request.POST["table"]
-        id = request.POST["id"]
-        if table == "contacts":
-            row = Contacts.objects.get(pk = id)
-        elif table == "donors":
-            row = Donors.objects.get(pk = id)
-        elif table == "sponsors":
-            row = Sponsors.objects.get(pk = id)
-        elif table == "students":
-            row = Students.objects.get(pk = id)
-        elif table == "volunteers":
-            row = Volunteers.objects.get(pk = id)
-        row.delete()
-    template = loader.get_template("delete.html")
-    context = {
-        "table" : table,
-        "id" : id
-    }
-    return HttpResponse(template.render(context,request))
+	return render(request, "search.html", context)
+#
+# def delete(request):
+#     if request.POST:
+#         table = request.POST["table"]
+#         id = request.POST["id"]
+#         if table == "contacts":
+#             row = Contacts.objects.get(pk = id)
+#         elif table == "donors":
+#             row = Donors.objects.get(pk = id)
+#         elif table == "sponsors":
+#             row = Sponsors.objects.get(pk = id)
+#         elif table == "students":
+#             row = Students.objects.get(pk = id)
+#         elif table == "volunteers":
+#             row = Volunteers.objects.get(pk = id)
+#         row.delete()
+#     template = loader.get_template("delete.html")
+#     context = {
+#         "table" : table,
+#         "id" : id
+#     }
+#     return HttpResponse(template.render(context,request))
