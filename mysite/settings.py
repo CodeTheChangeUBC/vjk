a679bc629cdf67b01aa13cdb0ba19b9280500ce1
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-05-25 21:53:34 -0700

    Still trying..

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Redirect to home URL after login 
