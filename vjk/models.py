5faf3c046ab468dae463a742232ad6756cbeb8c1
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-06-22 13:07:40 -0700

    Fixed errors with optional fields and changed button label.

--- vjk/models.py
+++ vjk/models.py
@@ -1,31 +1,35 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Contact(models.Model):
 	first_name 	= models.CharField(max_length = 75)
 	last_name 	= models.CharField(max_length = 75)
 	email 		= models.EmailField()
-	phone 		= models.CharField(max_length = 20)
+
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$',
+					message="Phone number must be entered in the format: '+16045555555'. Up to 15 digits allowed.")
+	phone 		= models.CharField(validators=[phone_regex],max_length = 20)
 
 
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name
 
 	def organization(self):
-		""" 
+		"""
 		Return donor(s) or sponsor(s) for which contact is the contact
 		"""
 		result = ''
 		d1,d2 = self.donor1.first(), self.donor2.first()
 		s1,s2 = self.sponsor1.first(), self.sponsor2.first()
 
-		if d1: 
+		if d1:
 			result += d1.org_name
 		if d2:
 			result += ' and ' + d2.org_name if d1 else d2.org_name
 		if s1:
 			result += ' and ' + s1.name if d1 or d2 else s1.name
-		if s2: 
+		if s2:
 			result += ' and ' + s2.name if d1 or d2 or s1 else s2.name
 		return result
 		organization.short_description = "Contact For?"
@@ -33,34 +37,34 @@ class Contact(models.Model):
 
 class Donor(models.Model):
 	org_name 			= models.CharField(max_length = 50, verbose_name="Organization Name")
-	location 			= models.CharField(max_length = 100, blank=True)
-	year_donation 		= models.IntegerField(verbose_name="Year", blank=True)
-	amount_donation 	= models.IntegerField(verbose_name="Amount Donated", blank=True)
-	primary_contact 	= models.ForeignKey(Contact, 
-								on_delete=models.PROTECT, 
+	location 			= models.CharField(max_length = 100, blank=True, null=True)
+	year_donation 		= models.IntegerField(verbose_name="Year", blank=True, null=True)
+	amount_donation 	= models.IntegerField(verbose_name="Amount Donated", blank=True, null=True)
+	primary_contact 	= models.ForeignKey(Contact,
+								on_delete=models.PROTECT,
 								related_name="donor1",
-								blank=True)
-	secondary_contact 	= models.ForeignKey(Contact, 
-								on_delete=models.PROTECT, 
-								related_name="donor2", 
-								blank=True)
-	
+								blank=True, null=True)
+	secondary_contact 	= models.ForeignKey(Contact,
+								on_delete=models.PROTECT,
+								related_name="donor2",
+								blank=True, null=True)
+
 
 	def __str__(self):
-		return self.org_name 
+		return self.org_name
 
 class Sponsor(models.Model):
 	name 				= models.CharField(max_length = 50)
 	service_provided 	= models.CharField(max_length = 50, blank=True)
-	primary_contact 	= models.ForeignKey(Contact, 
-								on_delete=models.PROTECT, 
-								related_name="sponsor1", 
-								default=-1, 
+	primary_contact 	= models.ForeignKey(Contact,
+								on_delete=models.PROTECT,
+								related_name="sponsor1",
+								default=-1,
 								blank=True)
-	secondary_contact 	= models.ForeignKey(Contact, 
-								on_delete=models.PROTECT, 
-								related_name="sponsor2", 
-								default=-1, 
+	secondary_contact 	= models.ForeignKey(Contact,
+								on_delete=models.PROTECT,
+								related_name="sponsor2",
+								default=-1,
 								blank=True)
 
 	def __str__(self):
@@ -74,9 +78,9 @@ class Student(models.Model):
 	location 			= models.CharField(max_length = 100, blank=True)
 	school 				= models.CharField(max_length = 75, blank=True)
 	year_attended 		= models.IntegerField()
-	reference_fname 	= models.CharField(max_length=75,default="", 
+	reference_fname 	= models.CharField(max_length=75,default="",
 										verbose_name="Reference First Name")
-	reference_lname 	= models.CharField(max_length=75,default="", 
+	reference_lname 	= models.CharField(max_length=75,default="",
 										verbose_name="Reference Last Name")
 	reference_email 	= models.EmailField(default="")
 
@@ -90,8 +94,7 @@ class Volunteer(models.Model):
 	email 				= models.EmailField()
 	phone 				= models.CharField(max_length  = 20)
 	role 				= models.CharField(max_length = 75, blank=True)
-	years_helped 		= models.CharField(max_length=75,default="", blank=True) 
+	years_helped 		= models.CharField(max_length=75,default="", blank=True)
 
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name
-
