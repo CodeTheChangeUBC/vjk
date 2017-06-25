8443007591f233ce300c507ba5334b01b1849163
bchugg <ben.ih.chugg@gmail.com>
2017-06-25 16:04:41 -0700

    fix delete button issue

--- vjk/admin.py
+++ vjk/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-#from django.contrib.auth.models import User
-#from django.contrib.sites.models import Site
 from django.contrib.auth.models import Group
 
-#admin.site.unregister(User)
 admin.site.unregister(Group)
-#admin.site.unregister(Site)
 
 from .models import Contact, Donor, Sponsor, Student, Volunteer
 
