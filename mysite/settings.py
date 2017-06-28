7e94e9f2709f71d75d69996434680283022d203f
leesw98 <win981026@gmail.com>
2017-06-28 15:34:13 +0900

    For now hardcoded columns (Thinking of using context for dynamic columns)

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,13 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Redirect to home URL after login
 LOGIN_REDIRECT_URL = '/'
--- static/optionschange.js
+++ static/optionschange.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     var arr = [".contacts", ".donors", ".sponsors", ".students", ".volunteers"];
     var tbl = "." + $("#table").val();
     var options = [
-    ["First Name", "Last Name", "Email", "Phone", "Donor", "Sponsor", "Student", "Volunteer"],
-    ["Org Name", "Location", "Year donation", "Amount Donated", "Primary Contact ID", "Secondary Contact ID"],
-    ["Name", "Service Provided"],
-    ["First Name", "Last Name", "Email", "Location", "School", "Year Attended", "Ref. First Name", "Ref. Last Name", "Ref. Email"],
-    ["First Name", "Last Name", "Email", "Phone", "Role", "Years Helped"]];
+    ["First Name", "Last Name", "Email", "Phone", "Address Line 1", "Address Line 2", "City", "Postal Code", "Province"],
+    ["Org Name", "Location", "Year Donated", "Amount Donated", "Primary Contact ID", "Secondary Contact ID"],
+    ["Name", "Service Provided", "Primary Contact ID", "Secondary Contact ID"],
+    ["First Name", "Last Name", "Email", "Phone Number", "Program City", "School", "Program Institution", "Year Attended", "Address Line 1", "Address Line 2", "City", "Province", "Postal Code", "Ref. First Name", "Ref. Last Name", "Ref. Email", "Ref. Address Line 1", "Ref. Address Line 2", "Ref. City", "Ref. Province"],
+    ["First Name", "Last Name", "Email", "Phone", "Role", "Years Helped", "Address Line 1", "Address Line 2", "City", "Postal Code", "Province"]];
     for (var i = 0; i < arr.length; i++) {
       if (tbl == arr[i]) {
         $("#field").empty();
