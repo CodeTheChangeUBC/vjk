493569f11f779a44c247cece762d7e9d6b88f9d8
leesw98 <win981026@gmail.com>
2017-06-28 14:52:53 +0900

    Don't change the static root path please. it breaks the static files :(

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Redirect to home URL after login
