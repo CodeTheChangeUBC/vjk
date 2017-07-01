1a75075abb1a64facce0e456ff2a7208271b6498
Angad Kalra <o1c9@ugrad.cs.ubc.ca>
2017-07-01 10:53:48 -0700

    Trying to make contributions data show up on index page. Not working yet...

--- vjk/views.py
+++ vjk/views.py
@@ -9,16 +9,18 @@ from .models import Contact, Donor, Sponsor, Student, Volunteer, Contribution
 
 @login_required(login_url='/admin/login/', redirect_field_name='redirect')
 def index(request):
-	contacts	 	= Contact.objects.values();
-	contact_fields 	= Contact._meta.get_fields()[5:]
-	donors 			= Donor.objects.values();
-	donor_fields 	= Donor._meta.get_fields()[1:]
-	sponsors 		= Sponsor.objects.values();
-	sponsor_fields 	= Sponsor._meta.get_fields()[1:]
-	students 		= Student.objects.values()
-	student_fields 	= Student._meta.get_fields()[1:]
-	volunteers 		= Volunteer.objects.values()
-	volunteer_fields= Volunteer._meta.get_fields()[1:]
+	contacts	 		= Contact.objects.values();
+	contact_fields 		= Contact._meta.get_fields()[5:]
+	donors 				= Donor.objects.values();
+	donor_fields 		= Donor._meta.get_fields()[1:]
+	sponsors 			= Sponsor.objects.values();
+	sponsor_fields 		= Sponsor._meta.get_fields()[1:]
+	students 			= Student.objects.values()
+	student_fields 		= Student._meta.get_fields()[1:]
+	volunteers 			= Volunteer.objects.values()
+	volunteer_fields	= Volunteer._meta.get_fields()[1:]
+	contributions 		= Contribution.objects.values()
+	contribution_fields = Contribution._meta.get_fields()[1:]
 
 	context = {
             "request": request,
@@ -32,5 +34,7 @@ def index(request):
 			"student_fields": student_fields,
 			"volunteers": volunteers,
 			"volunteer_fields": volunteer_fields,
+			"contributions": contributions,
+			"contribution_fields": contribution_fields
 	 }
 	return render(request, 'vjk/index.html', context)
